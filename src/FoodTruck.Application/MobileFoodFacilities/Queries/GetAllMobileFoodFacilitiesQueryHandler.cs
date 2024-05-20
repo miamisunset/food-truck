fd2642bb05cd8294afe301c246aa1edfe0f4771f
Alex Yermolaev <ayermolaev@pm.me>
2024-05-20 12:18:50 -0400

    🚧 Add GetAllMobileFoodFacilities query and handler
    This commit introduces the GetAllMobileFoodFacilitiesQuery class and its corresponding handler, GetAllMobileFoodFacilitiesQueryHandler. In addition, the 'ErrorOr' package has been added to the project dependencies.

---
+++ src/FoodTruck.Application/MobileFoodFacilities/Queries/GetAllMobileFoodFacilitiesQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+using FoodTruck.Domain.MobileFoodFacilities;
+using MediatR;
+
+namespace FoodTruck.Application.MobileFoodFacilities.Queries;
+
+public class GetAllMobileFoodFacilitiesQueryHandler 
+    : IRequestHandler<GetAllMobileFoodFacilitiesQuery, ErrorOr<List<MobileFoodFacility>>>
+{
+    public Task<ErrorOr<List<MobileFoodFacility>>> Handle(
+        GetAllMobileFoodFacilitiesQuery query, 
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
