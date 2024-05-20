9c498adced58080aa160d6a9e8e6f82ff49e19e4
Alex Yermolaev <ayermolaev@pm.me>
2024-05-20 12:54:37 -0400

    🚧 Add MobileFoodFacilitiesController and update dependencies
    Added a new controller 'MobileFoodFacilitiesController' for handling mobile food facility-related API calls. Also, removed unnecessary code from 'Program.cs' and updated the version of 'Swashbuckle.AspNetCore' dependency in 'FoodTruck.Api.csproj' file.

---
+++ src/FoodTruck.Api/Controllers/MobileFoodFacilitiesController.cs
@@ -0,0 +1,16 @@
+﻿using FoodTruck.Application.MobileFoodFacilities.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTruck.Api.Controllers;
+
+[Route("{controller}")]
+public class MobileFoodFacilitiesController(ISender mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllMobileFoodFacilities()
+    {
+        await mediator.Send(new GetAllMobileFoodFacilitiesQuery());
+        return Ok();
+    }
+}
