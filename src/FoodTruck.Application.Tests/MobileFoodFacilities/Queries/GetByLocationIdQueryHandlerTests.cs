13125b1ed3491da81cba41187e604be3b73b296a
Alex Yermolaev <ayermolaev@pm.me>
2024-05-21 10:28:04 -0400

    ✅ Add unit tests for MobileFoodFacilities queries
    Two new test classes are added for GetByLocationIdQuery and GetByNameQuery in the MobileFoodFacilities module. These tests check if the correct MobileFoodFacility is returned when a valid LocationId/ApplicantName exists, and if appropriate errors are returned when the LocationId/ApplicantName does not exist. Additionally, the test project has been added to the solution file.

---
+++ src/FoodTruck.Application.Tests/MobileFoodFacilities/Queries/GetByLocationIdQueryHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using ErrorOr;
+using FoodTruck.Application.MobileFoodFacilities.Queries;
+using FoodTruck.Application.Common.Interfaces;
+using FoodTruck.Domain.MobileFoodFacilities;
+using FluentAssertions;
+using Moq;
+
+namespace FoodTruck.Application.Tests.MobileFoodFacilities.Queries;
+
+public class GetByLocationIdQueryHandlerTests
+{
+    private readonly Mock<IMobileFoodFacilityRepository> _repositoryMock;
+    private readonly GetByLocationIdQueryHandler _handler;
+
+    public GetByLocationIdQueryHandlerTests()
+    {
+        _repositoryMock = new Mock<IMobileFoodFacilityRepository>();
+        _handler = new GetByLocationIdQueryHandler(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnMobileFoodFacility_WhenLocationIdExists()
+    {
+        // Arrange
+        var query = new GetByLocationIdQuery(1);
+        var cancellationToken = new CancellationToken();
+        var expectedFoodFacility = 
+            new MobileFoodFacility { LocationId = 1, Applicant = "ApplicantName" };
+
+        _repositoryMock.Setup(r => r.GetByLocationId(query.LocationId))
+            .ReturnsAsync(expectedFoodFacility);
+
+        // Act
+        var result = await _handler.Handle(query, cancellationToken);
+
+        // Assert
+        result.Value.Should().BeEquivalentTo(expectedFoodFacility);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnError_WhenLocationIdDoesNotExist()
+    {
+        // Arrange
+        var query = new GetByLocationIdQuery(1);
+        var cancellationToken = new CancellationToken();
+        MobileFoodFacility nullFoodFacility = null!;
+        
+        _repositoryMock.Setup(r => r.GetByLocationId(query.LocationId))
+            .ReturnsAsync(nullFoodFacility);
+        
+        // Act
+        var result = await _handler.Handle(query, cancellationToken);
+        
+        // Assert
+        result.FirstError.Should().Be(Error.NotFound(
+            $"{query.LocationId} mobile food facility not found"));
+    }
+}
---
+++ src/FoodTruck.Application.Tests/MobileFoodFacilities/Queries/GetByNameQueryHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using ErrorOr;
+using FluentAssertions;
+using FoodTruck.Application.Common.Interfaces;
+using FoodTruck.Application.MobileFoodFacilities.Queries;
+using FoodTruck.Domain.MobileFoodFacilities;
+using Moq;
+
+namespace FoodTruck.Application.Tests.MobileFoodFacilities.Queries;
+
+public class GetByNameQueryHandlerTests
+{
+    private readonly Mock<IMobileFoodFacilityRepository> _repositoryMock;
+    private readonly GetByNameQueryHandler _handler;
+
+    public GetByNameQueryHandlerTests()
+    {
+        _repositoryMock = new Mock<IMobileFoodFacilityRepository>();
+        _handler = new GetByNameQueryHandler(_repositoryMock.Object);
+    }
+    
+    [Fact]
+    public async Task Handle_ShouldReturnListOfMobileFoodFacility_WhenApplicantExists()
+    {
+        // Arrange
+        var query = new GetByNameQuery("ApplicantName");
+        var cancellationToken = new CancellationToken();
+        var expectedFoodFacilities = new List<MobileFoodFacility>();
+        expectedFoodFacilities.Add(new MobileFoodFacility { LocationId = 1, Applicant = "ApplicantName" });
+        expectedFoodFacilities.Add(new MobileFoodFacility { LocationId = 2, Applicant = "ApplicantName" });
+
+        _repositoryMock.Setup(r => r.GetByApplicant(query.Applicant))
+            .ReturnsAsync(expectedFoodFacilities);
+
+        // Act
+        var result = await _handler.Handle(query, cancellationToken);
+
+        // Assert
+        result.Value.Should().BeEquivalentTo(expectedFoodFacilities);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnError_WhenLocationIdDoesNotExist()
+    {
+        // Arrange
+        var query = new GetByNameQuery("ApplicantName");
+        var cancellationToken = new CancellationToken();
+        List<MobileFoodFacility> nullFoodFacility = null!;
+        
+        _repositoryMock.Setup(r => r.GetByApplicant(query.Applicant))
+            .ReturnsAsync(nullFoodFacility);
+        
+        // Act
+        var result = await _handler.Handle(query, cancellationToken);
+        
+        // Assert
+        result.FirstError.Should().Be(Error.NotFound(
+            description: "No food trucks or carts found"));
+    }
+}
